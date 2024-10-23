689beea71aa82766580704dc0ab3702eb1255f30
Hesselst <hesselsteenwoerd@gmail.com>
2024-10-23 21:00:18 +0200

    Added another way of storing letters, added color to console
    Will divide these in different classes soon because this is just not that practical

--- ConsoleLetters/Main.cpp
+++ ConsoleLetters/Main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 #include "Main.h"
 
 int main()
 {
+	// This part is just for changing the console's color (;
+
+	// Change int k to whatever color you want
+	int k = 2;
+	/*
+	1 = Dark blue
+	2 = Dark green
+	3 = Dark cyan
+	4 = Dark red
+	5 = Dark purple
+	6 = Dark yellow
+	7 = Dark white ???
+	8 = Dark grey
+	9 = bright blue
+	10 = bright green
+	11 = Bright cyan
+	12 = Bright red
+	13 = Bright purple
+	14 = Bright yellow
+	15 = Bright white
+
+	If you go over 15 the background color will change and i won't write all colors up to 255 so you will have to experiment 
+	with that yourself 
+	*/
+	HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, k);
+
 	// Get the input from the user
 	std::string input;
 	std::cout << "Type a word that you want to be displayed in the console here: ";
@@ -25,40 +54,46 @@ std::string ReturnBlockLetter(char letter)
 	// tolower function so i dont have to worry about capital letters
 	letter = std::tolower(letter);
 
+	// Better way of storing the letters
+	std::string a[6][1];
+
+	a[0][0] = "       ";
+	a[1][0] = "       ";
+	a[2][0] = "  __ _ ";
+	a[3][0] = " / _` |";
+	a[4][0] = "| (_| |";
+	a[5][0] = " \\__,_|";
+
+	std::string b[6][1];
+
+	b[0][0] = " _     ";
+	b[1][0] = "| |    ";
+	b[2][0] = "| |__  ";
+	b[3][0] = "| '_ \\ ";
+	b[4][0] = "| |_) |";
+	b[5][0] = "|_.__ /";
+
+	std::string question[6][1];
+	question[0][0] = " ___  ";
+	question[1][0] = "|__ \\ ";
+	question[2][0] = "   ) |";
+	question[3][0] = "  / / ";
+	question[4][0] = " |_|  ";
+	question[5][0] = " (_)  ";
+
 	// Just a check of letters below, will return the ascii art (not sure if ill finish this lool)
 
 	// Also still have to make it so every letter gets printed after each other and not below each other
 	if (letter == 'a')
 	{
-		return (
-			"  __ _\n"
-			" / _` |\n"
-			"| (_| |\n"
-			" \\\__,_|\n" // make sure to write \ like \\\ if you want them to be printed
-			);
+		return "a";
 	}
 	else if (letter == 'b')
 	{
-		return (
-			"_\n"
-			"| |\n"
-			"| |__\n"
-			"| '_ \\\ \n"
-			"| |_) |\n"
-			"|_.__ /\n"
-			);
+		return "b";
 	}
 	else
 	{
-		return (
-			" ___\n"
-			"|__ \\\ \n"
-			"   ) |\n"
-			"  / / \n"
-			" |_|  \n"
-			" (_)  \n"
-      
-      
-			);
+		return "?";
 	}
 }
