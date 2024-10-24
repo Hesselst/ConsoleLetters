088186f3062a8f1a09efc324dced40a26dad04c6
Hesselst <hesselsteenwoerd@gmail.com>
2024-10-24 15:51:15 +0200

    Fixed the error handling, works now, ReturnConsolColor completely works now

--- ConsoleLetters/Main.cpp
+++ ConsoleLetters/Main.cpp
@@ -26,52 +26,79 @@ int main()
 	return 0;
 }
 
+// Returns the color you want the console to be
 int ReturnConsoleColor()
 {
-	bool loopIsRunning = true;
-	std::string changeColorInput;
-	int changeColor;
+    bool loopIsRunning = true;
+    std::string changeColorInput;
+    int changeColor;
 
-	while (loopIsRunning)
-	{
+    while (loopIsRunning)
+    {
+        std::cout << "Do you want to change the color of the console? y/n" << std::endl;
+        std::getline(std::cin, changeColorInput);
 
-		std::cout << "Do you want to change the color of the console? y/n" << std::endl;
-		std::getline(std::cin, changeColorInput);
+        // Convert input to lowercase
+        for (auto& c : changeColorInput)
+        {
+            c = std::tolower(c);
+        }
 
-		changeColorInput = std::tolower(changeColorInput);
+        if (changeColorInput == "y")
+        {
+            bool innerLoopRunning = true;
+            while (innerLoopRunning)
+            {
+                try
+                {
+                    std::cout << "What color do you want the console to be?\n"
+                        << "1 = Darkblue\n2 = Dark green\n3 = Dark cyan\n4 = Dark red\n"
+                        << "5 = Dark purple\n6 = Dark yellow\n7 = Dark white\n8 = Dark grey\n"
+                        << "9 = Bright blue\n10 = Bright green\n11 = Bright cyan\n12 = Bright red\n"
+                        << "13 = Bright purple\n14 = Bright yellow\n15 = Bright white\n"
+                        << "If you go over 15, the background color will change. "
+                        << "Experiment with that yourself up to 255." << std::endl;
 
-		if (changeColorInput == "y")
-		{
-			try
-			{
-				std::cout << "What color do you want the console to be?\n1 = Darkblue\n2 = Dark green\n3 = Dark cyan\n4 = Dark red\n5 = Dark purple\n6 = Dark yellow\n7 = Dark white ? ? ?\n8 = Dark grey\n9 = bright blue\n10 = bright green\n11 = Bright cyan\n12 = Bright red\n13 = Bright purple\n14 = Bright yellow\n15 = Bright white\nIf you go over 15 the background color will change and i won't write all colors up to 255 so you will have to experiment with that yourself " << std::endl;
-				std::getline(std::cin, changeColorInput);
-				changeColor = std::stoi(changeColorInput);
+                    std::getline(std::cin, changeColorInput);
+                    // String to int function
+                    changeColor = std::stoi(changeColorInput);
 
-				if (changeColor < 0 || changeColor > 255) 
-				{
-					std::cout << "Please enter a number between 0 and 255" << std::endl;
-				}
-				else {
-					return changeColor;
-				}
-			}
-			catch (...) 
-			{
-				std::cerr << "An unexpected error occurred.\n";
-			}
-			return changeColor;
-		}
-		else if (changeColorInput == "n")
-		{
-			loopIsRunning = false;
-		}
-		else
-		{
-			std::cout << "Please enter a valid number" << std::endl;
-		}
+                    if (changeColor < 0 || changeColor > 255)
+                    {
+                        std::cout << "Please enter a number between 0 and 255" << std::endl;
+                    }
+                    else
+                    {
+                        return changeColor;
+                    }
+                }
+                // Error handling catch the errors here
+                catch (const std::invalid_argument&)
+                {
+                    std::cerr << "Invalid input. Please enter a number.\n" << std::endl;
+                }
+                catch (const std::out_of_range&)
+                {
+                    std::cerr << "Please enter a number between 0 and 255.\n" << std::endl;
+                }
+                catch (...)
+                {
+                    std::cerr << "An unexpected error occurred.\n";
+                }
+            }
+        }
+        else if (changeColorInput == "n")
+        {
+            loopIsRunning = false;
+        }
+        else
+        {
+            std::cout << "Please enter only y or n\n" << std::endl;
+        }
+    }
 
-	}
+    // -1 because no color change
+    return -1;
 }
 
 // Returns a string
