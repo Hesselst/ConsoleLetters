a2742dad276f88b2705616f1aad8caca7cfd1888
Hesselst <hesselsteenwoerd@gmail.com>
2024-10-26 21:16:31 +0200

    Organized a bit

--- ConsoleLetters/Main.h
+++ ConsoleLetters/Main.h
@@ -1,7 +1,12 @@
+
+// This header file is only for main.cpp
+
 #pragma once
 
-std::string ReturnBlockLetter(char letter);
-
-int ReturnConsoleColor();
+// All includes necesarry for main
+#include "CommonIncludes.h"
+#include <windows.h>
+#include "BlockLetters.h"
+#include "Returns.h"
 
 void MainLoop();
